889f6343bcb7b505541b392f706933ac4667b3fd
AmanChaudhary1998 <chaudharyaman50@yahoo.com>
2020-09-27 11:46:14 +0530

    Updates

---
+++ precache-manifest.1081aeb6afa4f408a3f82f78b54201de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfc043074f4a22e55ca610eebf624da",
+    "url": "/git@github.com:AmanChaudhary1998/React-SPA-monsters.git/index.html"
+  },
+  {
+    "revision": "3bad1c2f23f579c28438",
+    "url": "/git@github.com:AmanChaudhary1998/React-SPA-monsters.git/static/css/main.499b423d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/git@github.com:AmanChaudhary1998/React-SPA-monsters.git/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:AmanChaudhary1998/React-SPA-monsters.git/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bad1c2f23f579c28438",
+    "url": "/git@github.com:AmanChaudhary1998/React-SPA-monsters.git/static/js/main.0649088d.chunk.js"
+  },
+  {
+    "revision": "f1c57b6e4e5be078e129",
+    "url": "/git@github.com:AmanChaudhary1998/React-SPA-monsters.git/static/js/runtime-main.9cc0539c.js"
+  }
+]);
